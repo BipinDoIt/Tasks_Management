1d603b89ec93964b727d590021055231294ca1cf
Bipin <bipin19285@gmail.com>
2024-07-22 09:47:28 +0530

    Task_Management

--- mySetuEHSNirvanaBE/app/database.py
+++ mySetuEHSNirvanaBE/app/database.py
@@ -29,7 +29,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_TEST_DATABASE_URL = "postgresql://postgres:8182@localhost/Bipindb10_test"
+SQLALCHEMY_TEST_DATABASE_URL = "postgresql://postgres:8182@localhost/Bipindb10"
 engine = create_engine(SQLALCHEMY_TEST_DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
